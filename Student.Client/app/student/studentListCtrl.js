6dbf7663df4bf004250a2b20409d7b6a13dc1baf
pantony89 <pantony89@gmail.com>
2017-07-11 07:35:11 -0700

    Student-Client

---
+++ Student.Client/app/student/studentListCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("studentManagement")
+        .controller("studentListCtrl", ["studentResource", studentListCtrl]);
+
+    function studentListCtrl(studentResource) {
+        var vm = this;
+        debugger;
+        studentResource.query(function (data) {
+            vm.students = data;
+        });      
+    }
+}());
---
+++ Student.Client/common/studentResource.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("studentResource",
+                ["$resource",
+                 "appSettings",
+                    studentResource])
+
+    function studentResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/Student");
+    }
+}());
+
